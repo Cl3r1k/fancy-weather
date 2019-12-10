1718657cde00a08f70cc7777f5db6ecbf7ec3f08
Cl3r1k <Cl3r1k2@gmail.com>
2019-12-10 16:05:10 +0700

    feat: basic API request

--- fancy-weather/src/index.js
+++ fancy-weather/src/index.js
@@ -1,7 +1,31 @@
+/* eslint-disable no-console */
 import './scss/main.scss';
 
-const initApp = () => {
-  //
+const geoPosition = require('./js/geoPosition');
+const weather = require('./js/weather');
+const image = require('./js/image');
+const map = require('./js/map');
+
+const initApp = async () => {
+  // Get Position Data
+  const geoPositionData = await geoPosition.getGeoPosition();
+  const cityName = geoPositionData.city;
+  const countryName = geoPositionData.country;
+  console.log('geoPositionData', geoPositionData);
+  console.log('cityName', cityName, 'countryName', countryName);
+  const [latitude, longitude] = [...geoPositionData.loc.split(',')];
+  console.log('latitude', latitude, 'longitude', longitude);
+
+  // Get Weather Data
+  const weatherData = await weather.getWeatherDataByPosition(latitude, longitude);
+  console.log('weatherData', weatherData);
+
+  // Get Image Data
+  const imageData = await image.getImageUrl();
+  console.log('imageData', imageData);
+
+  // Set Map Data
+  map.setMapPosition(latitude, longitude);
 };
 
 document.addEventListener('DOMContentLoaded', () => {
