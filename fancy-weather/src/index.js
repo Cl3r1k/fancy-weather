a65f2d8aed8c934f98db6ae4d0b1282e523ca4b1
Cl3r1k <Cl3r1k2@gmail.com>
2019-12-11 15:52:58 +0700

    feat: implement switch image and switch °C/°F

--- fancy-weather/src/index.js
+++ fancy-weather/src/index.js
@@ -1,41 +1,21 @@
-/* eslint-disable no-console */
 import './scss/main.scss';
 
-const geoPosition = require('./js/geoPosition');
-const weather = require('./js/weather');
-const image = require('./js/image');
+const settings = require('./js/settings');
 const map = require('./js/map');
 const domHelper = require('./js/domHelper');
 
 const initApp = async () => {
-  // Get Position Data
-  const geoPositionData = await geoPosition.getGeoPosition();
-  const cityName = geoPositionData.city;
-  const countryName = geoPositionData.country;
-  console.log('geoPositionData', geoPositionData);
-  console.log('cityName', cityName, 'countryName', countryName);
-  const [latitude, longitude] = [...geoPositionData.loc.split(',')];
-  console.log('latitude', latitude, 'longitude', longitude);
+  await domHelper.generateAppData();
 
-  // Get Weather Data
-  const weatherData = await weather.getWeatherDataByPosition(latitude, longitude);
-  // TODO: Do not forget to remove localStorage part
-  // localStorage.setItem('weatherData', JSON.stringify(weatherData));
-  // const weatherData = JSON.parse(localStorage.getItem('weatherData'));
-  console.log('weatherData', weatherData);
+  domHelper.updateAppView();
 
   // Get Image Data
-  const imageData = await image.getImageUrl();
-  // localStorage.setItem('imageData', JSON.stringify(imageData));
-  // TODO: Do not forget to remove localStorage part
-  // const imageData = JSON.parse(localStorage.getItem('imageData'));
-  console.log('imageData', imageData);
+  await domHelper.changeBackgroundImage();
 
   // Set Map Data
-  map.setMapPosition(latitude, longitude);
+  map.setMapPosition(settings.latitude, settings.longitude);
 
-  const isCelsius = true;
-  domHelper.updateAppView(cityName, countryName, latitude, longitude, weatherData, isCelsius);
+  domHelper.setDOMHandlers();
 };
 
 document.addEventListener('DOMContentLoaded', () => {
--- fancy-weather/src/js/domHelper.js
+++ fancy-weather/src/js/domHelper.js
@@ -1,8 +1,33 @@
-/* eslint-disable no-console */
-const updateAppView = (cityName, countryName, latitude, longitude, weatherData, isCelsius) => {
+const settings = require('./settings');
+const image = require('./image');
+const geoPosition = require('./geoPosition');
+
+// Not related to DOM changes/events, consider to move to another file
+const generateAppData = async () => {
+  // Get Position Data
+  settings.geoPositionData = await geoPosition.getGeoPosition();
+  settings.cityName = settings.geoPositionData.city;
+  settings.countryName = settings.geoPositionData.country;
+  // console.log('geoPositionData', geoPositionData);
+  // console.log('cityName', cityName, 'countryName', countryName);
+  const [latitude, longitude] = [...settings.geoPositionData.loc.split(',')];
+  settings.latitude = latitude;
+  settings.longitude = longitude;
+  // console.log('latitude', latitude, 'longitude', longitude);
+
+  // Get Weather Data
+  // settings.weatherData = await weather.getWeatherDataByPosition(latitude, longitude);
+  // TODO: Do not forget to remove localStorage part
+  // localStorage.setItem('weatherData', JSON.stringify(settings.weatherData));
+  settings.weatherData = JSON.parse(localStorage.getItem('weatherData'));
+  // console.log('settings.weatherData', settings.weatherData);
+};
+// ------------ Not related to DOM changes --------------------
+
+const updateAppView = () => {
   // Set City Data
   const cityNameElement = document.getElementById('idCityName');
-  cityNameElement.textContent = `${cityName}, ${countryName}`;
+  cityNameElement.textContent = `${settings.cityName}, ${settings.countryName}`;
 
   // Set time
   const currentDateTimeElement = document.getElementById('idCurrentDateTime');
@@ -15,26 +40,26 @@ const updateAppView = (cityName, countryName, latitude, longitude, weatherData, 
 
   // Set geo position
   const coordinatesElement = document.getElementById('idCoordinates');
-  const [lat, long] = [latitude, longitude].map(coordinate => `${coordinate}'`.replace('.', `°`));
+  const [lat, long] = [settings.latitude, settings.longitude].map(coordinate => `${coordinate}'`.replace('.', `°`));
   coordinatesElement.innerHTML = `Latitude: ${lat} <br> Longitude: ${long}`;
 
   // Set weather data
-  const currentTemperature = isCelsius
-    ? parseInt((weatherData.currently.temperature - 32) / 1.8)
-    : parseInt(weatherData.currently.temperature);
-  console.log('currentTemperature', currentTemperature);
-  const feelsLike = isCelsius
-    ? parseInt((weatherData.currently.apparentTemperature - 32) / 1.8)
-    : parseInt(weatherData.currently.apparentTemperature);
-  console.log('feelsLike', feelsLike);
-  const { windSpeed, humidity, icon } = weatherData.currently;
-  console.log('windSpeed', windSpeed);
-  console.log('humidity', `${humidity * 100}%`);
-  console.log('icon', icon);
+  const currentTemperature = settings.isCelsius
+    ? parseInt((settings.weatherData.currently.temperature - 32) / 1.8)
+    : parseInt(settings.weatherData.currently.temperature);
+  // console.log('currentTemperature', currentTemperature);
+  const feelsLike = settings.isCelsius
+    ? parseInt((settings.weatherData.currently.apparentTemperature - 32) / 1.8)
+    : parseInt(settings.weatherData.currently.apparentTemperature);
+  // console.log('feelsLike', feelsLike);
+  const { windSpeed, humidity, icon } = settings.weatherData.currently;
+  // console.log('windSpeed', windSpeed);
+  // console.log('humidity', `${humidity * 100}%`);
+  // console.log('icon', icon);
 
   // Set weather data for Current day
   const weatherCardDetailedElement = document.querySelector('.weather-card-detailed');
-  console.log('weatherCardDetailedElement', weatherCardDetailedElement);
+  // console.log('weatherCardDetailedElement', weatherCardDetailedElement);
   weatherCardDetailedElement.querySelector('.weather-card-temperature').textContent = `${currentTemperature}°`;
   weatherCardDetailedElement.querySelector(
     '.weather-card-extra-info',
@@ -47,22 +72,48 @@ const updateAppView = (cityName, countryName, latitude, longitude, weatherData, 
   const weatherCardsElements = document.querySelectorAll('.weather-future .weather-card');
   // console.log('weatherCardsElements', weatherCardsElements);
   weatherCardsElements.forEach((weatherCard, index) => {
-    console.log('weatherData.daily.data[index]', weatherData.daily.data[index + 1]);
-    const weekDay = new Date(weatherData.daily.data[index + 1].time * 1000).toLocaleDateString('en-EN', {
+    // console.log('weatherData.daily.data[index]', weatherData.daily.data[index + 1]);
+    const weekDay = new Date(settings.weatherData.daily.data[index + 1].time * 1000).toLocaleDateString('en-EN', {
       weekday: 'long',
     });
     // console.log('weekDay', weekDay);
-    const temperature = isCelsius
-      ? parseInt((weatherData.daily.data[index + 1].temperatureMin - 32) / 1.8)
-      : parseInt(weatherData.daily.data[index + 1].temperatureMin);
+    const temperature = settings.isCelsius
+      ? parseInt((settings.weatherData.daily.data[index + 1].temperatureMin - 32) / 1.8)
+      : parseInt(settings.weatherData.daily.data[index + 1].temperatureMin);
     weatherCard.querySelector('.weather-card-day').textContent = weekDay;
     weatherCard.querySelector('.weather-card-temperature').textContent = `${temperature}°`;
     weatherCard
       .querySelector('.weather-card-icon')
-      .setAttribute('src', `./assets/images/weather_icons/${weatherData.daily.data[index + 1].icon}.png`);
+      .setAttribute('src', `./assets/images/weather_icons/${settings.weatherData.daily.data[index + 1].icon}.png`);
   });
+};
+
+const changeBackgroundImage = async () => {
+  const imageData = await image.getImageUrl();
+  // localStorage.setItem('imageData', JSON.stringify(imageData));
+  // TODO: Do not forget to remove localStorage part
+  // const imageData = JSON.parse(localStorage.getItem('imageData'));
+  // console.log('imageData', imageData);
+  // console.log('imageData.urls.regular', imageData.urls.regular);
+  document.getElementById('idBGImage').style.background = `url("${imageData.urls.regular}") no-repeat`;
+  document.getElementById('idBGImage').style.backgroundSize = 'cover';
+};
+
+const changeTemperatureScale = async () => {
+  settings.isCelsius = !settings.isCelsius;
+  updateAppView();
+};
+
+const setDOMHandlers = () => {
+  document.getElementById('idSwitchImageButton').addEventListener('click', changeBackgroundImage);
+  // document.getElementById('idSwitchLanguageButton').addEventListener('click', changeBackgroundImage);
+  document.getElementById('idSwitchTemperatureButton').addEventListener('click', changeTemperatureScale);
 };
 
 module.exports = {
   updateAppView,
+  setDOMHandlers,
+  changeBackgroundImage,
+  changeTemperatureScale,
+  generateAppData,
 };
